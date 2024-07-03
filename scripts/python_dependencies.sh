aace0472081e2db60305700575049065c44edd1e
Stewie2k46 <stewie2k956@gmail.com>
2024-07-03 19:13:52 +0530

    Update python_dependencies.sh

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo chown -R ubuntu:ubuntu ~/Project_folder_name
-virtualenv /home/ubuntu/Project_folder_name/venv
-source /home/ubuntu/Project_folder_name/venv/bin/activate
-pip install -r /home/ubuntu/Project_folder_name/requirements.txt
+sudo chown -R ubuntu:ubuntu ~/django-pro
+virtualenv /home/ubuntu/django-pro/venv
+source /home/ubuntu/django-pro/venv/bin/activate
+pip install -r /home/ubuntu/django-pro/requirements.txt
